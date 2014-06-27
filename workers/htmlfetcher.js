41bc2c9f280e6fe2a1cc2d8167151bfefe527d6c
nelsonriley <nelsonriley@gmail.com>
2014-06-26 19:03:30 -0700

    Chron job instructions

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -9,7 +9,15 @@ var archive = require('../helpers/archive-helpers');
 archive.readListOfUrls(archive.downloadUrls);
 
 
-// setting up chron job
+// setting up chron job in 5 minutes
+// crontab -e    // to create a new crontab file
+// crontab -l    // to view current crontab files
+// Argument 1: Minute (0 - 59)
+// Argument 2: Hour (0 - 23)
+// Argument 3: Day of Month (1 - 31)
+// Argument 4: Month (1-12)
+// Argument 5: Day of Week (0 - 6) Sunday = 0
+// Argument 6: Command
 //
 // 3 parts : chron instructions - node path - file path
 // */1* * * *
